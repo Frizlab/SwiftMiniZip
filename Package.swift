d18e03c1cc89adf2b8a2607fad763939490f9777
Frizlab <fload@me.com>
2019-05-24 17:30:49 +0200

    Fix the minizip packages for Linux

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
 	products: [.library(name: "MiniZip", targets: ["MiniZip"])],
 	dependencies: [],
 	targets: [
-		.systemLibrary(name: "CMiniZip", pkgConfig: "minizip", providers: [.apt(["minizip"]), .brew(["minizip"])]),
+		.systemLibrary(name: "CMiniZip", pkgConfig: "minizip", providers: [.apt(["libminizip-dev", "zlib1g-dev"]), .brew(["minizip"])]),
 
 		.target(name: "MiniZip", dependencies: ["CMiniZip"]),
 		.testTarget(name: "MiniZipTests", dependencies: ["MiniZip"])
